dd0e1cf5ff26c6a041269b6215e85ef2f1af506c
Dmitry Mordanov <mordanov314@gmail.com>
2022-09-13 00:35:40 +0300

    1-4 task complete

--- task04.py
+++ task04.py
@@ -4,3 +4,26 @@
 # Пример:
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
+from random import *
+
+def my_pretty_print(poly): # красиво переводим полином в строку по всем правилам
+    ret = ""
+    for i in range(len(poly)-1,-1,-1): # идем по списку в обратном направлении
+        if poly[i]!=0:                 # нулевые коэффициенты не обрабатываем
+            ret += "+" if poly[i] > 0 else "-"  # знаки выводим отдельно
+            if (abs(poly[i])!=1) or (i==0):     # печатаем коэффициент, если не 1. Последний коэффициент выводим всегда
+                ret += f"{abs(poly[i])}"        # без знака
+            if (abs(poly[i])!=1) and (i>0): ret += "*"  # выводим знак * (если степень>0)
+            if i>0: ret += "x"
+            if i>1: ret += f"^{i}"
+    return ret[1:] if (ret[0:1] == "+") else ret  # убираем самый первый + если он есть
+
+# Степень многочлена
+k = 5
+#seed(4)
+polynom = [randint(-9,10) for _ in range(k+1)]
+
+print(polynom)
+print(my_pretty_print(polynom))
+with open("task04.txt","w") as f:
+    f.write(my_pretty_print(polynom))
--- task05.py
+++ task05.py
@@ -1,2 +1,39 @@
 # Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
+
+def my_pretty_print(poly): # красиво переводим полином в строку по всем правилам
+    ret = ""
+    for i in range(len(poly)-1,-1,-1): # идем по списку в обратном направлении
+        if poly[i]!=0:                 # нулевые коэффициенты не обрабатываем
+            ret += "+" if poly[i] > 0 else "-"  # знаки выводим отдельно
+            if (abs(poly[i])!=1) or (i==0):     # печатаем коэффициент, если не 1. Последний коэффициент выводим всегда
+                ret += f"{abs(poly[i])}"        # без знака
+            if (abs(poly[i])!=1) and (i>0): ret += "*"  # выводим знак * (если степень>0)
+            if i>0: ret += "x"
+            if i>1: ret += f"^{i}"
+    return ret[1:] if (ret[0:1] == "+") else ret  # убираем самый первый + если он есть
+
+def my_parse_poly(s):
+    ret = []
+    s = s.replace("-","+-")
+    if s[0:1] == "+": s = s[1:]
+    ret = s.split("+")
+
+    print(s)
+    return ret
+
+with open("task05_1.txt","r") as f:
+    str1 = f.read()
+
+with open("task05_2.txt","r") as f:
+    str2 = f.read()
+
+print("Прочитано из файлов:")
+print(str1)
+print(str2)
+print()
+poly1 =my_parse_poly(str1)
+#poly2 =my_parse_poly(str2)
+# print("Коэффициенты полиномов:")
+print(poly1)
+#print(poly2)
